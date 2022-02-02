df52f9b980f0003a268f971f845e1a29e0090a9e
Leon <leonardolatorre2000@gmail.com>
2022-02-02 18:18:36 +0100

    render method added, general class test

--- grid.rb
+++ grid.rb
@@ -7,6 +7,16 @@ class Grid
         @grid_size = grid_size
     end
 
+    def [](pos)
+        x, y = pos
+        @grid[x][y]
+    end
+
+    def []=(pos, value)
+        x,y = pos
+        @grid[x][y] = value
+    end
+
     def fill_grid
         @grid = @grid.each_with_index do |row,x|
             row.each_with_index do |tile,y|
@@ -16,5 +26,26 @@ class Grid
         end
     end
 
+    def place_bombs
+        bombs_quantity = 9
+        bombs_in_the_grid = 0
+        until bombs_in_the_grid == bombs_quantity
+            rand_x, rand_y = rand(0...@grid_size), rand(0...@grid_size)
+            if @grid[rand_x][rand_y] != "B"
+                @grid[rand_x][rand_y] = "B" 
+                bombs_in_the_grid += 1
+            end
+        end
+    end
+
+    def render
+        @grid.each do |row| 
+            arr = []
+            row.each do |tile|
+                arr << tile
+            end
+            puts arr.join(" ")
+        end 
+    end
 
 end
