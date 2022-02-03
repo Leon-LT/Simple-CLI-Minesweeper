20852bee587661c2f507e4592b46d734e3070676
Leon <leonardolatorre2000@gmail.com>
2022-02-03 16:17:17 +0100

    added methods: toggle_flag, reveal, neighbours. Fixed some typos

--- tile.rb
+++ tile.rb
@@ -1,24 +1,32 @@
 class Tile
-    def initialize(position, grid, bombed? = false)
-        @flagged? = false
-        @bombed? = bombed?
-        @revealed? = false
-        @nearest_bomb = 0
+    def initialize(position, grid, bombed = false)
+        @flagged = false
+        @bombed = bombed
+        @revealed = false
+        @neighbours_number = 0
         @position = position
         @grid = grid
     end
 
-    def flag
-        @flagged? = @flagged? ? false : true 
+    def toggle_flag
+        @flagged = @flagged ? false : true 
     end
 
     def reveal
-        @revealed? = @revealed? ? false : true 
+        @revealed = @revealed ? false : true 
     end
 
     def neighbours
-        neighbours_tiles = []
-        #finire metodo, deve salvare tutte le tile vicine nell'array
-    end
+        neighbours_coords = [[-1,0], [1,0], [0,-1], [0,1], [-1,-1], [-1,1], [1,-1], [1,1]]
+        neighbours = []
 
+        neighbours_coords.each do |coords|
+            x,y = coords
+            curr_x,curr_y = @position
+            puts x + curr_x
+            puts y + curr_y
+            puts "--------"
+            neighbours << @grid[x + curr_x][y + curr_y]
+        end
+    end
 end
